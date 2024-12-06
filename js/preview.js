e8ec4b8154f2cd1bc53b058e8d0ae91ce6c8be75
SergeyMedved <serezham13@gmail.com>
2024-12-06 20:07:13 +0700

    Поправил нейминг, добавил деструктуризацию объекта

--- js/preview.js
+++ js/preview.js
@@ -1,24 +1,24 @@
 const appendPhotos = (data) => {
 
-  const pictures = document.querySelector('.pictures');
+  const picturesElement = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content;
-  const photos = new DocumentFragment();
+  const photosFragment = new DocumentFragment();
 
-  data.forEach((element) => {
+  data.forEach(({id, url, description, likes, comments}) => {
     const clonedPicture = pictureTemplate.cloneNode(true);
-    const img = clonedPicture.querySelector('.picture__img');
-    const likes = clonedPicture.querySelector('.picture__likes');
-    const comments = clonedPicture.querySelector('.picture__comments');
-    img.src = element.url;
-    img.alt = element.description;
-    likes.textContent = element.likes;
-    comments.textContent = element.comments.length;
-    photos.appendChild(clonedPicture);
+    const imgElement = clonedPicture.querySelector('.picture__img');
+    const likesElement = clonedPicture.querySelector('.picture__likes');
+    const commentsElement = clonedPicture.querySelector('.picture__comments');
+    imgElement.dataset.id = id;
+    imgElement.src = url;
+    imgElement.alt = description;
+    likesElement.textContent = likes;
+    commentsElement.textContent = comments.length;
+    photosFragment.appendChild(clonedPicture);
   });
 
-  pictures.appendChild(photos);
+  picturesElement.appendChild(photosFragment);
 
 };
 
 export { appendPhotos };
-
