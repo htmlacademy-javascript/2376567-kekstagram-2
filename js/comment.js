11d786360c72389974fea1228813cd4ea14e66bc
SergeyMedved <serezham13@gmail.com>
2024-12-14 13:15:28 +0700

    вынес логику отрисовки комментов в отдельный модуль

--- js/comment.js
+++ js/comment.js
@@ -1,27 +1,63 @@
 const bodyElement = document.querySelector('body');
 const bigPictureElement = bodyElement.querySelector('.big-picture');
-const commentElement = document.querySelector('.social__comment');
+const commentElement = bigPictureElement.querySelector('.social__comment');
 const socialCommentsElement = bigPictureElement.querySelector('.social__comments');
+const commentShownCount = bigPictureElement.querySelector('.social__comment-shown-count');
+const commentTotalElement = bigPictureElement.querySelector('.social__comment-total-count');
+const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
 
+const STEP_SHOWN_COMMENTS = 5;
 
-const renderCommentsElement = (comments) => {
-  socialCommentsElement.textContent = null;
+const commentsLoader = (data) => {
+  let visibleComments = STEP_SHOWN_COMMENTS;
+  return () => {
+    const shownComments = data.slice(0, visibleComments);
+    commentShownCount.textContent = shownComments.length;
+    visibleComments += STEP_SHOWN_COMMENTS;
+    return shownComments;
+  };
+};
+
+const createCommentsElement = (comments) => {
   const socialsFragment = new DocumentFragment();
-  if (comments.length === 0) {
-    socialCommentsElement.textContent = null;
-  }
   comments.forEach(({ avatar, name, message }) => {
-    const clonnedCommentElement = commentElement.cloneNode(true);
-    const socialPictureElement = clonnedCommentElement.querySelector('.social__picture');
-    const socialTextElement = clonnedCommentElement.querySelector('.social__text');
+    const clonedCommentElement = commentElement.cloneNode(true);
+    const socialPictureElement = clonedCommentElement.querySelector('.social__picture');
+    const socialTextElement = clonedCommentElement.querySelector('.social__text');
 
     socialPictureElement.src = avatar;
     socialPictureElement.alt = name;
     socialTextElement.textContent = message;
 
-    socialsFragment.appendChild(clonnedCommentElement);
+    socialsFragment.appendChild(clonedCommentElement);
   });
   socialCommentsElement.appendChild(socialsFragment);
+};
+
+const updateVisibilityOfLoaderButton = (comments) => {
+  commentShownCount.textContent = comments.length;
+  if (commentShownCount.textContent === commentTotalElement.textContent) {
+    commentsLoaderElement.classList.add('hidden');
+  } else {
+    commentsLoaderElement.classList.remove('hidden');
+  }
+};
+
+const renderCommentsElement = (data) => {
+  socialCommentsElement.textContent = null;
+  const addComments = commentsLoader(data);
+  const renderedComments = addComments();
+  updateVisibilityOfLoaderButton(renderedComments);
+  createCommentsElement(renderedComments);
+
+  const onCommentsLoaderElement = () => {
+    socialCommentsElement.textContent = null;
+    const newComments = addComments();
+    createCommentsElement(newComments);
+    updateVisibilityOfLoaderButton(newComments);
+  };
+
+  commentsLoaderElement.addEventListener('click', onCommentsLoaderElement);
 };
 
 export { renderCommentsElement };
--- js/photo.js
+++ js/photo.js
@@ -6,46 +6,15 @@ const imgElement = bigPictureElement.querySelector('.big-picture__img img');
 const socialCaptionElement = bigPictureElement.querySelector('.social__caption');
 const likesCountElement = bigPictureElement.querySelector('.likes-count');
 const commentTotalElement = bigPictureElement.querySelector('.social__comment-total-count');
-const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
-const commentShownCount = bigPictureElement.querySelector('.social__comment-shown-count');
 
-const STEP_SHOWN_COMMENTS = 5;
-
-const commentsLoader = () => {
-  let visibleComments = STEP_SHOWN_COMMENTS;
-  return (data) => {
-    const shownComments = data.slice(0, visibleComments);
-    commentShownCount.textContent = shownComments.length;
-    visibleComments += STEP_SHOWN_COMMENTS;
-
-    if (commentShownCount.textContent === commentTotalElement.textContent) {
-      commentsLoaderElement.classList.add('hidden');
-    } else {
-      commentsLoaderElement.classList.remove('hidden');
-    }
-
-    return shownComments;
-  };
-};
 
 const renderBigPicture = (photos, index) => {
   const { url, description, likes, comments } = photos[index];
-
   imgElement.src = url;
   socialCaptionElement.textContent = description;
   likesCountElement.textContent = likes;
   commentTotalElement.textContent = comments.length;
-
-  const loadComments = commentsLoader();
-
-  const onCommentsLoaderElementClick = () => {
-    const loadedComments = loadComments(comments);
-    renderCommentsElement(loadedComments);
-  };
-
-  onCommentsLoaderElementClick();
-
-  commentsLoaderElement.addEventListener('click', onCommentsLoaderElementClick);
+  renderCommentsElement(comments);
 };
 
 export { renderBigPicture };
