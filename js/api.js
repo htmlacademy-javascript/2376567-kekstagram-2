74472c3f73c364a0acd6ea57b6496f2c66132039
Sergey <serezham13@gmail.com>
2025-01-15 21:37:08 +0700

    11.14. Надо подкачаться

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const DATA_PATH = 'https://31.javascript.htmlacademy.pro/kekstagram';
+const GET_ROUTE = '/data';
+const POST_ROUTE = '/';
+
+
+const load = async (path, route, method, body = null) => {
+  const response = await fetch(`${path}${route}`, {
+    method: method,
+    body: body
+  });
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+  throw new Error(`Ошибка загрузки: ${response.status}`);
+};
+
+const postData = (body) => load(DATA_PATH, POST_ROUTE, 'POST', body);
+const getData = async () => {
+  const data = await load(DATA_PATH, GET_ROUTE, 'GET');
+  return data;
+};
+
+export { postData, getData };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-import { createPhotos } from './data.js';
+// import { createPhotos } from './data.js';
+import { getData } from './api.js';
 import { renderPhotos } from './preview.js';
 import { renderModal } from './modal.js';
 import { loadForm } from './form.js';
-import './edit-picture.js';
 
-const data = createPhotos(25);
-renderPhotos(data);
-renderModal(data);
+const ERROR_SHOWN_TIME = 5000;
+
+const onErrorGet = () => {
+  const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+  const errorBlockElement = dataErrorTemplate.cloneNode(true);
+  document.body.insertAdjacentElement('beforeBegin', errorBlockElement);
+  setTimeout(() => errorBlockElement.remove(), ERROR_SHOWN_TIME);
+};
+
 loadForm();
+
+try {
+  const data = await getData();
+  renderPhotos(data);
+  renderModal(data);
+} catch (error) {
+  onErrorGet();
+}
